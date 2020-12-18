067834b8471d2321d49ec3a60ab453f5efaaec2e
adamoc <oceallaighadam96@gmail.com>
2020-12-18 23:00:39 +0000

    Fifth Commit , one more thing to do with uncompleted ones now

--- app/src/main/java/com/example/listplanner/ViewSingleTask.java
+++ app/src/main/java/com/example/listplanner/ViewSingleTask.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import DB.DB;
@@ -21,6 +25,7 @@ public class ViewSingleTask extends AppCompatActivity {
     TextView txtViewTitle , txtViewDescription , txtViewDate, txtViewPriority , txtViewSingleDueDate;
     Button btnUpdate;
     ImageButton btnDelete;
+    Spinner spStatus;
     FloatingActionButton btnClose;
     DB myDB;
 
@@ -42,8 +47,24 @@ public class ViewSingleTask extends AppCompatActivity {
             txtViewTitle.setText(foundTask.getTitle());
             txtViewPriority.setText(String.valueOf(foundTask.getPriority()));
             txtViewDescription.setText(foundTask.getDescription());
+            //Setting the spinner values to be of the enum status type of the task model, so user can update status
+            spStatus.setAdapter(new ArrayAdapter<Task.StatusType>(this, android.R.layout.simple_spinner_item, Task.StatusType.values()));
+            spStatus.setSelection(Arrays.asList(Task.StatusType.values()).indexOf(Enum.valueOf(Task.StatusType.class, foundTask.getStatus().toString())));
             txtViewDate.setText("Created at: " + foundTask.getDate());
             txtViewSingleDueDate.setText("Due Date: " + foundTask.getdueDate());
+
+            spStatus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Object item = parent.getSelectedItem();
+                    checkStatus(Enum.valueOf(Task.StatusType.class, item.toString()) , foundTask);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
 
             // Brings the app to update page to update the respective task
             btnUpdate.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +97,34 @@ public class ViewSingleTask extends AppCompatActivity {
         }
     }
 
+    // Passing in my Enum and taking the necessary and appropriate action based on the result of the enum
+    public void checkStatus(Task.StatusType status , Task foundTask){
+        HashMap<Task.StatusType, Runnable> statuses = new HashMap<Task.StatusType , Runnable>(){{
+            put(Task.StatusType.ToDo , new Runnable(){
+                @Override
+                public void run() {
+                    return;
+                }
+            });
+            put(Task.StatusType.InProgress , new Runnable(){
+                @Override
+                public void run() {
+                    myDB.updateStatus(foundTask.getId() , String.valueOf(status));
+                    return;
+                }
+            });
+            put(Task.StatusType.Completed , new Runnable(){
+                @Override
+                public void run() {
+                    myDB.deleteTask(foundTask);
+                    Toast.makeText(ViewSingleTask.this , "Task Deleted Successfully" , Toast.LENGTH_LONG).show();
+                    backToMainScreen();
+                }
+            });
+        }};
+        statuses.get(status).run();
+    }
+
     public void backToMainScreen(){
         Intent backToMainScreen = new Intent(ViewSingleTask.this , MainActivity.class);
         startActivity(backToMainScreen);
@@ -91,6 +140,7 @@ public class ViewSingleTask extends AppCompatActivity {
         btnUpdate = (Button) findViewById(R.id.btnUpdate);
         btnClose = (FloatingActionButton) findViewById(R.id.btnClose);
         btnDelete = (ImageButton) findViewById(R.id.btnDelete);
+        spStatus = (Spinner) findViewById(R.id.spStatus);
     }
 
     // Helper function to identify the priority level and pass back the corresponding colour with that
